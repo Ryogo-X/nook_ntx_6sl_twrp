6ef6d1f0d0ed06923947724a56297ba26c4b5e59
Ryogo <ryogo.z@gmail.com>
2018-08-31 00:55:11 +0300

    - initial commit;

---
+++ omni_nook_ntx_6sl.mk
@@ -0,0 +1,9 @@
+# Inherit configuration
+$(call inherit-product, build/target/product/full.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := nook_ntx_6sl
+PRODUCT_NAME := omni_nook_ntx_6sl
+PRODUCT_BRAND := BarnesAndNoble
+PRODUCT_MODEL := Nook eBook
+PRODUCT_MANUFACTURER := Barnes and Noble
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_nook_ntx_6sl-userdebug
+
